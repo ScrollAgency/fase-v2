69d41d61557f3ccaaccf0ba56970e02be2daede5
Gael <prigentgael@gmail.com>
2025-05-21 19:01:37 +0200

    add analitics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import "@uppy/dashboard/dist/style.min.css";
 import '@/styles/globals.css';
 import "@/styles/fonts.css";
 
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
