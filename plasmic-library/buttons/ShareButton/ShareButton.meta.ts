d746d23bf9f1817695f5a1b7ea4d80597f194db6
clement <clementpain@outlook.com>
2025-06-17 10:49:23 +0200

    add props to share button

--- plasmic-library/buttons/ShareButton/ShareButton.meta.ts
+++ plasmic-library/buttons/ShareButton/ShareButton.meta.ts
@@ -3,7 +3,19 @@ export const ShareButtonMeta = {
    name: "ShareButton",
    displayName: "Bouton de Partage",
    props: {
-      children: 'slot'
+      children: 'slot',
+      title: {
+         type: "string",
+         description: "Titre du lien de partage",
+      },
+      description: {
+         type: 'string',
+         description: "Texte descriptif du lien"
+      },
+      image: {
+         type: 'string',
+         description: "image à afficher avec le lien"
+      }
    },
    importPath: "./components/buttons/ShareButton/ShareButton",
 };
--- plasmic-library/buttons/ShareButton/ShareButton.tsx
+++ plasmic-library/buttons/ShareButton/ShareButton.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 
 interface ShareButtonProps {
-
   className?: string; // Classe CSS pour personnaliser le bouton
   children?: React.ReactNode; // Contenu pour le variant link
+  title?: string; // Titre personnalisé pour le partage
+  description?: string; // Description personnalisée pour le partage
+  image?: string; // URL de l'image à partager
 }
 
 const ShareButton: React.FC<ShareButtonProps> = ({
   className = "",
-  children
+  children,
+  title,
+  description,
+  image
 }) => {
   const handleShare = async () => {
     if (typeof window !== "undefined" && navigator.share) {
       try {
-        await navigator.share({
-          title: document.title,
+        const shareData: ShareData = {
+          title: title || document.title,
+          text: description,
           url: window.location.href,
-        });
+        };
+
+        if (image) {
+          const response = await fetch(image);
+          const blob = await response.blob();
+          const file = new File([blob], 'shared-image.jpg', { type: blob.type });
+          shareData.files = [file];
+        }
+
+        await navigator.share(shareData);
       } catch (err) {
         console.error("Erreur lors du partage :", err);
       }
