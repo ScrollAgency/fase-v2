379d6ea9daac29e1d5f8a746f87edf70e4454dba
clement <clementpain@outlook.com>
2025-06-18 09:22:11 +0200

    new props button google

--- plasmic-library/buttons/ButtonGoogle/ButtonGoogle.meta.ts
+++ plasmic-library/buttons/ButtonGoogle/ButtonGoogle.meta.ts
@@ -5,39 +5,13 @@ const ButtonGoogleMeta = {
     description: "Google authentication button",
     thumbnailUrl: "https://static1.plasmic.app/insertables/button.svg",
     props: {
-      label: "string",
-      icon: {
-        type: "choice",
-        defaultValue: "none",
-        options: ["start", "end", "only", "none"],
-        required: false,
-      },
-      destructive: "boolean",
-      hierarchy: {
-        type: "choice",
-        defaultValue: "primary",
-        options: ["primary", "secondary"],
-        required: false,
-      },
-      size: {
-        type: "choice",
-        defaultValue: "large",
-        options: ["small", "large"],
-        required: false,
-      },
-      state: {
-        type: "choice",
-        defaultValue: "default",
-        options: ["default", "hover", "focused", "disabled"],
-        required: false,
-      },
-      iconImage: "imageUrl",
-      className: "string",
+      disabled: "boolean",
       redirectTo: {
         type: "string",
         description: "slug of the page to redirect the user after authentication (without first /)",
         diplayName: "redirect slug"
-      }
+      },
+      children: "slot"
     },
     importPath: "./components/forms/ButtonGoogle/ButtonGoogle",
   };
