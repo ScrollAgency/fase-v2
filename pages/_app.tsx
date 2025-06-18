da6ad93476366f143e48878db97c71e581164aff
clement <clementpain@outlook.com>
2025-06-18 16:33:00 +0200

    remove more context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import type { AppProps } from 'next/app';
-import { PlasmicRootProvider } from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from '@/plasmic-init';
 
 import "@uppy/core/dist/style.min.css";
 import "@uppy/dashboard/dist/style.min.css";
@@ -9,16 +7,10 @@ import "@/styles/fonts.css";
 
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { useAuthInit } from '@/plasmic-library/authentication/useAuthInit';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { user, loading } = useAuthInit();
-
-  // This will run on every page load and automatically initialize the session
-  console.log('Auth state:', { user, loading });
-
   return (
-      <Component {...pageProps} />
+    <Component {...pageProps} />
   )
 }
 
